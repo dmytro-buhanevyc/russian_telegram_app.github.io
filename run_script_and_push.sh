edba5978624219cf6a09ad78d9485509cad48652
Dima Buhanevyc <dimabuhanevyc@FO-TCA-DBukhanevych-mac.local>
2023-11-19 22:07:31 +0200

    update

---
+++ run_script_and_push.sh
@@ -0,0 +1,10 @@
+
+# Run your Python script
+/usr/bin/python3 /Users/dimabuhanevyc/Documents/Research_Analysis/TCA/telegram_analysis/Ukraine_Telegram/telegrams_ukraine_updated.py
+
+# Navigate to your repository and execute Git commands
+cd /Users/dimabuhanevyc/Documents/Research_Analysis/russian_telegram_app.github.io
+git add .
+git commit -m "update"
+git push origin main
+
